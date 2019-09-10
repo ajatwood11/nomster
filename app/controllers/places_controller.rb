a4cb60da7b4648e386037363d87c2691b787821c
Austin Atwood <austinatwoodjs30@gmail.com>
2019-09-10 08:39:14 +0000

    valid

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-    before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+    before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   # a bunch of other code
 
   def index
@@ -11,8 +11,12 @@ class PlacesController < ApplicationController
   end
 
  def create
-  current_user.places.create(place_params)
-  redirect_to root_path
+ @place = current_user.places.create(place_params)
+  if @place.valid?
+    redirect_to root_path
+  else
+    render :new, status: :unprocessable_entity
+  end
 end
 
 def show
@@ -33,12 +37,21 @@ def update
   if @place. user != current_user
     return render plain: 'Not Allowed', status: :forbidden
   end
+
   @place.update_attributes(place_params)
-  redirect_to root_path
-end
+  if @place.valid?
+    redirect_to root_path
+  else
+    render :edit, status: :unprocessable_entity
+  end
+end 
+
   
 def destroy
   @place = Place.find(params[:id])
+   if @place.user != current_user
+    return render plain: 'Not Allowed', status: :forbidden
+  end
   @place.destroy
   redirect_to root_path
 end
