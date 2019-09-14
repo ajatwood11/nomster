c8fd2ca9d0f159d1377454d6f7b1665882db700f
Austin Atwood <austinatwoodjs30@gmail.com>
2019-09-14 17:08:15 +0000

    comments

--- db/migrate/20190914170138_create_comments.rb
+++ db/migrate/20190914170138_create_comments.rb
@@ -1,9 +1,11 @@
 class CreateComments < ActiveRecord::Migration[5.2]
   def change
     create_table :comments do |t|
+       t.text :message
       t.string :rating
       t.integer :user_id
       t.integer :place_id
+      t.timestamps
       t.timestamps
     end
     add_index :comments, [:user_id, :place_id]
