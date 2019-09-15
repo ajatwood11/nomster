1fe7391ccdd4afbb53bed2754f79a66d7060c63f
Austin Atwood <austinatwoodjs30@gmail.com>
2019-09-15 04:09:45 +0000

    map

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_11_014404) do
+ActiveRecord::Schema.define(version: 2019_09_14_170138) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
+    t.text "message"
     t.string "rating"
     t.integer "user_id"
     t.integer "place_id"
